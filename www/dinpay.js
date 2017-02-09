0623e413426ff06b6e31803889e0275ace85463c
dawning <dawning7670@gmail.com>
2017-02-09 10:28:39 +0800

    fix

--- www/dinpay.js
+++ www/dinpay.js
@@ -3,6 +3,9 @@ var className = "DinPay";
 var DinPay = {
     pay: function (success, err, param) {
         exec(success, err, className, "pay", [param]);
+    },
+    payWithXML: function (success, err, param) {
+        exec(success, err, className, "payWithXML", [param]);
     }
 }
 module.exports = DinPay;
